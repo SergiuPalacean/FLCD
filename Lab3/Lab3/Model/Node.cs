cc12eb6b545ff561681572a4b0ef7093ce5497ea
Sergiu Palacean <32163378+SergiuPalacean@users.noreply.github.com>
2021-10-20 07:32:58 +0300

    Add files via upload

---
+++ Lab3/Lab3/Model/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3.Model
+{
+    class Node
+    {
+        public string Data { get; set; }
+        public int Index { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(string data)
+        {
+            Data = data;
+            Left = Right = null;
+        }
+    }
+}
---
+++ Lab3/Lab3/Model/SymbolTable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3.Model
+{
+    class SymbolTable
+    {
+        private Node root;
+        private int index;
+
+        public SymbolTable()
+        {
+            root = null;
+            index = 0;
+        }
+
+        public int Add(string id)
+        {
+            Node node = new Node(id);
+            if(root is null)
+            {
+                root = node;
+                root.Index = index;
+                index++;
+                return root.Index;
+            }
+            else
+            {
+                Node aux = root;
+                while(!aux.Data.Equals(id))
+                {
+                    if(id.CompareTo(aux.Data)<0)
+                    {
+                        if(aux.Left!=null)
+                        {
+                            aux = aux.Left;
+                        }
+                        else
+                        {
+                            aux.Left = node;
+                            aux.Left.Index = index;
+                            index++;
+                        }
+                    }
+                    else
+                    {
+                        if (aux.Right != null)
+                        {
+                            aux = aux.Right;
+                        }
+                        else
+                        {
+                            aux.Right = node;
+                            aux.Right.Index = index;
+                            index++;
+                        }
+                    }
+                }
+                return aux.Index;
+            }
+        }
+    }
+}
